13d421ff6552e04bb89c19b934ab102a7c136d87
Peter Yoo <trustintofu@gmail.com>
2022-10-13 17:45:28 -0700

    wip: removes theme changing buttons

--- components/header.components.tsx
+++ components/header.components.tsx
@@ -1,4 +1,4 @@
-import { BsSun } from "react-icons/bs";
+import { ThemeButton } from "./themeButton.components";
 
 export const Header = () => {
   return (
@@ -6,12 +6,7 @@ export const Header = () => {
       <section className="flex pb-[40px] items-center justify-between py-[28px]">
         <h1 className="text-2xl font-bold">Home</h1>
         <div className="flex gap-3 items-center">
-          <button>
-            <BsSun />
-          </button>
-          <button>
-            <h6 className="text-sm">Light Mode</h6>
-          </button>
+          <ThemeButton />
         </div>
       </section>
     </>
