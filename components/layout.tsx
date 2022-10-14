06ca321694ed9311203295b2e6270f10b022e38b
Peter Yoo <trustintofu@gmail.com>
2022-10-13 17:28:43 -0700

    wip: adds meta tag, removes bg color

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,8 +6,9 @@ export const Layout = ({ children }: any) => {
     <>
       <Head>
         <title>Top 16 Stock Prices | NextJS / Tailwind</title>
+        <meta name="description" content="Display top 16 stock prices" />
       </Head>
-      <div className="min-h-screen min-w-screen bg-[#F3F8FF] flex flex-col">
+      <div className="min-h-screen min-w-screen flex flex-col">
         <Header />
         <main className="flex-grow">{children}</main>
       </div>
