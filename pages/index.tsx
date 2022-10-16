2f39652daf2bb56efb5838e17ff3a4c7b6a76a8a
Peter Yoo <trustintofu@gmail.com>
2022-10-16 00:01:25 -0700

    wip: gets our api token from our hidden env file, adds useQuery hook to fetch our data. We only need 16 stock prices so we slice 16 elements from the returned data object and map them to display our jsx.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,28 @@
+import { QueryClient, useQuery } from "@tanstack/react-query";
 import { Card } from "../components/card.components";
 
 export default function Home() {
+  const token = process.env.NEXT_PUBLIC_TOKEN;
+
+  const { isLoading, isError, error, data }: any = useQuery(
+    ["stock"],
+    async () => {
+      return fetch(
+        `https://finnhub.io/api/v1/stock/symbol?exchange=US&token=${token}`
+      ).then((res) => res.json());
+    }
+  );
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2>{error.message}</h2>;
+  }
+
+  console.log(data);
+
   return (
     <>
       <section className="py-[28px]">
@@ -10,6 +32,21 @@ export default function Home() {
           <Card />
         </section>
       </section>
+      {data.slice(0, 15).map((stock: any) => (
+        <div key={stock.id}>{stock.symbol}</div>
+      ))}
     </>
   );
 }
+
+// export const getServerSideProps = async () => {
+//   const queryClient = new QueryClient();
+
+//   await queryClient.prefetchQuery(["stock"], () => {
+//     fetch("").then((res) => res.json());
+//   });
+
+//   return {
+//     props: {},
+//   };
+// };
