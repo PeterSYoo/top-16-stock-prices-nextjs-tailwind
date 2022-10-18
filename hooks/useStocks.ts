63441e1e5f95ec40c6d7c6568df6c31828656df6
Peter Yoo <trustintofu@gmail.com>
2022-10-18 00:08:20 -0700

    wip: adds Promise.all to correctly map the passed in array to our data fetchers. Removes our react query functions from our hook.

--- hooks/useStocks.ts
+++ hooks/useStocks.ts
@@ -1,55 +1,21 @@
-import { useQueries } from "@tanstack/react-query";
-
 const token = process.env.NEXT_PUBLIC_TOKEN;
 
-const fetchStocks = (symbol: any) => {
-  return fetch(
-    `https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${token}`
+export const getStocks = async (tickers: any) => {
+  return await Promise.all(
+    tickers.map(async (symbol: any) => {
+      return fetch(
+        `https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${token}`
+      ).then((res) => res.json());
+    })
   );
 };
 
-const fetchCompanyName = (symbol: any) => {
-  return fetch(
-    `https://finnhub.io/api/v1/stock/profile2?symbol=${symbol}&token=${token}`
+export const getStocksName = async (tickers: any) => {
+  return await Promise.all(
+    tickers.map(async (symbol: any) => {
+      return fetch(
+        `https://finnhub.io/api/v1/stock/profile2?symbol=${symbol}&token=${token}`
+      ).then((res) => res.json());
+    })
   );
-};
-
-export const useStocks = (symbols: any) => {
-  symbols?.map((symbol: any, index: any) => {
-    return useQueries({
-      queries: [
-        {
-          queryKey: ["quote", symbol],
-          queryFn: () => fetchStocks(symbol),
-          retry: false,
-          onSuccess: (data: any) => {
-            console.log("Succesfully fetched", data, index);
-          },
-          onError: (error: any) => {
-            console.log("There was an error", error, index);
-          },
-        },
-      ],
-    });
-  });
-};
-
-export const useStockName = (symbols: any) => {
-  symbols?.map((symbol: any, index: any) => {
-    return useQueries({
-      queries: [
-        {
-          queryKey: ["company-name", symbol],
-          queryFn: () => fetchCompanyName(symbol),
-          retry: false,
-          onSuccess: (data: any) => {
-            console.log("Succesfully fetched", data, index);
-          },
-          onError: (error: any) => {
-            console.log("There was an error", error, index);
-          },
-        },
-      ],
-    });
-  });
 };
