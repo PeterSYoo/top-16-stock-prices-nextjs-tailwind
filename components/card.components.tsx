91732e75679f707fba385862f2d678e07690017c
Peter Yoo <trustintofu@gmail.com>
2022-10-17 22:09:51 -0700

    wip: setups card component to be ready to take in the fetched data.

--- components/card.components.tsx
+++ components/card.components.tsx
@@ -1,22 +1,24 @@
-export const Card = () => {
+export const Card = ({ tickers, result, index, name }: any) => {
   return (
     <>
       <div className="bg-white dark:bg-black w-[325px] h-[174px] rounded-lg shadow shadow-gray-400 dark:shadow-black dark:shadow-lg flex flex-col justify-between p-[20px]">
         {/* Top Row */}
         <div className="flex justify-between items-center">
           <div className="flex flex-col">
-            <h1 className="font-bold">AAPL</h1>
-            <h6 className="text-xs font-thin">Apple</h6>
+            <h1 className="font-bold">Stock Symbol</h1>
+            <h6 className="text-xs font-thin">Stock Name</h6>
           </div>
-          <h1 className="font-bold">$23,738</h1>
+          <h1 className="font-bold">Current Price</h1>
         </div>
         {/* Bottom Row */}
         <div className="flex justify-between items-center">
           <h6 className="text-xs font-thin">Daily</h6>
           <div className="flex gap-4 items-center">
-            <h6 className="text-[#049C6B] text-xs font-bold">+$556.14</h6>
+            <h6 className="text-[#049C6B] text-xs font-bold">Price Change</h6>
             <div className="bg-[#E8FFEB] dark:bg-[#2A4037] rounded-3xl">
-              <h6 className="text-[#049C6B] text-xs font-bold p-1">+64.11%</h6>
+              <h6 className="text-[#049C6B] text-xs font-bold p-1">
+                Percent Price Change
+              </h6>
             </div>
           </div>
         </div>
