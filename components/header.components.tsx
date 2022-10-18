94904dc7bf0f3fdf5251f41718c57de72fa9f19e
Peter Yoo <trustintofu@gmail.com>
2022-10-18 00:13:36 -0700

    wip: Fixes x padding on our header.

--- components/header.components.tsx
+++ components/header.components.tsx
@@ -3,7 +3,7 @@ import { ThemeButton } from "./themeButton.components";
 export const Header = () => {
   return (
     <>
-      <section className="flex pb-[40px] items-center justify-between py-[28px]">
+      <section className="flex pb-[40px] items-center justify-between py-[28px] px-[20px]">
         <h1 className="text-2xl font-bold">Home</h1>
         <div className="flex gap-3 items-center">
           <ThemeButton />
