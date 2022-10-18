47769de5d403331b43d4ac038bc4219920181cba
Peter Yoo <trustintofu@gmail.com>
2022-10-17 22:10:55 -0700

    wips: imports our hook functions for fetching data, adds an array of top 16 companies to be passed into the custom hooks. Maps the results and pass the data as props to our card component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,47 @@
-import { QueryClient, useQuery } from "@tanstack/react-query";
 import { Card } from "../components/card.components";
+import { useStockName, useStocks } from "../hooks/useStocks";
+
+const tickers = [
+  "AAPL",
+  "MSFT",
+  "AMZN",
+  "TSLA",
+  "GOOGL",
+  "GOOG",
+  "BRK.B",
+  "UNH",
+  "JNJ",
+  "XOM",
+  "JPM",
+  "META",
+  "V",
+  "PG",
+  "NVDA",
+  "HD",
+];
 
 export default function Home() {
-  const token = process.env.NEXT_PUBLIC_TOKEN;
-
-  const { isLoading, isError, error, data }: any = useQuery(
-    ["stock"],
-    async () => {
-      return fetch(
-        `https://finnhub.io/api/v1/stock/symbol?exchange=US&token=${token}`
-      ).then((res) => res.json());
-    }
-  );
-
-  if (isLoading) {
-    return <h2>Loading...</h2>;
-  }
-
-  if (isError) {
-    return <h2>{error.message}</h2>;
-  }
-
-  console.log(data);
+  const queryResults: any | unknown = useStocks(tickers);
+  const nameResults: any | unknown = useStockName(tickers);
 
   return (
     <>
       <section className="py-[28px]">
         {/* Card Container */}
         <section className="flex flex-wrap gap-[9px] justify-center">
-          {/* Card */}
-          <Card />
+          {queryResults?.map((result: any, index: any) => (
+            <div key={index}>
+              {/* Card */}
+              <Card
+                tickers={tickers}
+                result={result}
+                index={index}
+                name={nameResults[index]}
+              />
+            </div>
+          ))}
         </section>
       </section>
-      {data.slice(0, 15).map((stock: any) => (
-        <div key={stock.id}>{stock.symbol}</div>
-      ))}
     </>
   );
 }
-
-// export const getServerSideProps = async () => {
-//   const queryClient = new QueryClient();
-
-//   await queryClient.prefetchQuery(["stock"], () => {
-//     fetch("").then((res) => res.json());
-//   });
-
-//   return {
-//     props: {},
-//   };
-// };
